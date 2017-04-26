b9ed6ea83b860284893dadf89352051f01b77247
Steve Cox <stevecoxiscool@gmail.com>
2017-04-26 00:29:59 -0500

    Google Cloud Speech API Example Using Winforms C#
    Simple Winforms C#  example to test Google Cloud Speech API

---
+++ WinRecognize/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WinRecognize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRecordVoice = new System.Windows.Forms.Button();
+            this.btnSpeechInfo = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnPlayAudio = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRecordVoice
+            // 
+            this.btnRecordVoice.Location = new System.Drawing.Point(12, 12);
+            this.btnRecordVoice.Name = "btnRecordVoice";
+            this.btnRecordVoice.Size = new System.Drawing.Size(75, 23);
+            this.btnRecordVoice.TabIndex = 0;
+            this.btnRecordVoice.Text = "Record Voice";
+            this.btnRecordVoice.UseVisualStyleBackColor = true;
+            this.btnRecordVoice.Click += new System.EventHandler(this.btnRecordVoice_Click);
+            // 
+            // btnSpeechInfo
+            // 
+            this.btnSpeechInfo.Location = new System.Drawing.Point(12, 70);
+            this.btnSpeechInfo.Name = "btnSpeechInfo";
+            this.btnSpeechInfo.Size = new System.Drawing.Size(100, 23);
+            this.btnSpeechInfo.TabIndex = 1;
+            this.btnSpeechInfo.Text = "Convert Speech";
+            this.btnSpeechInfo.UseVisualStyleBackColor = true;
+            this.btnSpeechInfo.Click += new System.EventHandler(this.btnSpeechInfo_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 41);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 99);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 150);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btnPlayAudio
+            // 
+            this.btnPlayAudio.Location = new System.Drawing.Point(118, 41);
+            this.btnPlayAudio.Name = "btnPlayAudio";
+            this.btnPlayAudio.Size = new System.Drawing.Size(100, 23);
+            this.btnPlayAudio.TabIndex = 4;
+            this.btnPlayAudio.Text = "Play Audio";
+            this.btnPlayAudio.UseVisualStyleBackColor = true;
+            this.btnPlayAudio.Visible = false;
+            this.btnPlayAudio.Click += new System.EventHandler(this.btnPlayAudio_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnPlayAudio);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSpeechInfo);
+            this.Controls.Add(this.btnRecordVoice);
+            this.Name = "Form1";
+            this.Text = "Win Recognize";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRecordVoice;
+        private System.Windows.Forms.Button btnSpeechInfo;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnPlayAudio;
+    }
+}
+
---
+++ WinRecognize/Form1.cs
@@ -0,0 +1,175 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Speech.V1;
+using Grpc.Auth;
+
+
+//Install-Package Google.Cloud.Speech.V1 -Version 1.0.0-beta08 -Pre
+
+namespace WinRecognize
+{
+    public partial class Form1 : Form
+    {
+
+        private BufferedWaveProvider bwp;
+
+        WaveIn waveIn;
+        WaveOut waveOut;
+        WaveFileWriter writer;
+        WaveFileReader reader; 
+        string output = "audio.raw";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            waveOut = new WaveOut();
+            waveIn = new WaveIn();
+
+            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(waveIn_DataAvailable);
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(16000, 1);
+            bwp = new BufferedWaveProvider(waveIn.WaveFormat);
+            bwp.DiscardOnBufferOverflow = true;
+
+            btnRecordVoice.Enabled = true;
+            btnSave.Enabled = false;
+            btnSpeechInfo.Enabled = false;
+
+
+
+        }
+        
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            bwp.AddSamples(e.Buffer, 0, e.BytesRecorded);
+
+        }
+
+        private void btnRecordVoice_Click(object sender, EventArgs e)
+        {
+            if (NAudio.Wave.WaveIn.DeviceCount < 1)
+            {
+                Console.WriteLine("No microphone!");
+                return;
+            }
+
+            waveIn.StartRecording();
+
+            btnRecordVoice.Enabled = false;
+            btnSave.Enabled = true;
+            btnSpeechInfo.Enabled = false;
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            waveIn.StopRecording();
+
+            if (File.Exists("audio.raw"))
+                File.Delete("audio.raw");
+
+            writer = new WaveFileWriter(output, waveIn.WaveFormat);
+
+            btnRecordVoice.Enabled = false;
+            btnSave.Enabled = false;
+            btnSpeechInfo.Enabled = true;
+
+            byte[] buffer = new byte[bwp.BufferLength];
+            int offset = 0;
+            int count = bwp.BufferLength;
+
+            var read = bwp.Read(buffer, offset, count);
+            if (count > 0)
+            {
+                writer.Write(buffer, offset, read);
+            }
+
+            waveIn.Dispose();
+            waveIn = null;
+            writer.Close();
+            writer = null;
+            
+            reader = new WaveFileReader("audio.raw"); // (new MemoryStream(bytes));
+            waveOut.Init(reader);
+            waveOut.PlaybackStopped += new EventHandler<StoppedEventArgs>(waveOut_PlaybackStopped);
+            waveOut.Play();
+            
+
+        }
+
+        private void waveOut_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            waveOut.Stop();
+            reader.Close();
+            reader = null;
+        }
+
+        private void btnSpeechInfo_Click(object sender, EventArgs e)
+        {
+
+            btnRecordVoice.Enabled = true;
+            btnSave.Enabled = false;
+            btnSpeechInfo.Enabled = false;
+
+            if (File.Exists("audio.raw"))
+            {
+
+                var speech = SpeechClient.Create();
+                var response = speech.Recognize(new RecognitionConfig()
+                {
+                    Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                    SampleRateHertz = 16000,
+                    LanguageCode = "en",
+                }, RecognitionAudio.FromFile("audio.raw"));
+
+
+                textBox1.Text = "";
+
+                foreach (var result in response.Results)
+                {
+                    foreach (var alternative in result.Alternatives)
+                    {
+                        textBox1.Text = textBox1.Text + " " + alternative.Transcript;
+                    }
+                }
+
+                if(textBox1.Text.Length == 0)
+                    textBox1.Text = "No Data ";
+
+            }
+            else
+            {
+
+                textBox1.Text = "Audio File Missing ";
+
+            }
+            
+
+        }
+
+        private void btnPlayAudio_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("audio.raw"))
+            { 
+                reader = new WaveFileReader("audio.raw"); 
+                waveOut.Init(reader);
+                waveOut.Play();
+            }
+            else
+            {
+                MessageBox.Show("No Audio File Found");
+            }
+        }
+    }
+}
